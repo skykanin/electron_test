0eb4a3c626b74db722f49c45b269911453146284
skykanin <3789764+skykanin@users.noreply.github.com>
2018-02-12 10:53:41 +0100

    added snapshots to jest testing

--- src/renderer/components/Welcome/Welcome.test.js
+++ src/renderer/components/Welcome/Welcome.test.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import Welcome from './Welcome'
 
 test('Welcome component should render as expected', () => {
     const wrapper = shallow(<Welcome compiler="Javascript" framework="React" />);
+    const tree = toJson(wrapper);
+    expect(tree).toMatchSnapshot();
 })
-
-/* describe('<Welcome />', () => {
-    it('renders 1 <Welcome /> component', () => {
-        const component = shallow(<Welcome compiler="Javascript" framework="React" />);
-        expect(component).toHaveLength(1);
-    })
-}) */
